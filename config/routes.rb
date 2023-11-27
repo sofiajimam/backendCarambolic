6c335eb3f609935f5173fb338595915b5759e6fb
sofiajimam <sofyjmtz@hotmail.com>
2023-11-27 14:39:58 +0100

    [ADD] routes: +healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: redirect("/healthcheck", status: 302)
+  get "/healthcheck", to: proc { [200, {}, ["success"]] }
   post "/graphql", to: "graphql#execute"
   # Session routes
   post "/login", to: "sessions#create"
